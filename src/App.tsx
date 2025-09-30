2d29b8c440200c0a1c8b4c118035eea5886113b0
umiso785 <umiso7885@gmail.com>
2025-10-01 00:16:22 +0900

    이미지와 정확히 일치하는 IDE 레이아웃 구현

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import IDELayout from "./layouts/IDELayout";
 import ExecutionDashboard from "./pages/ExecutionDashboard";
 import MainDashboard from "./pages/MainDashboard";
 import Settings from "./pages/Settings";
-
 export default function App() {
   return (
     <BrowserRouter>
@@ -11,7 +10,7 @@ export default function App() {
         <Route path="/" element={<Navigate to="/execution" replace />} />
         <Route path="/execution" element={<ExecutionDashboard />} />
         <Route path="/design" element={<IDELayout />} />
-        <Route path="/main" element={<MainDashboard />} />
+        <Route path="/design" element={<IDELayout />} />
         <Route path="/settings" element={<Settings />} />
         <Route path="/ide" element={<IDELayout />} />
       </Routes>
